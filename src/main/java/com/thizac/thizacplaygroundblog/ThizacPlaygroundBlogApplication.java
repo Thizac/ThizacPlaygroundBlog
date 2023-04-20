817cdff69daea4811d2954dbfdf8d6a9d7d0ee68
thizac <kuba6328224@gmail.com>
2023-04-20 20:01:11 +0200

    first commit

---
+++ src/main/java/com/thizac/thizacplaygroundblog/ThizacPlaygroundBlogApplication.java
@@ -0,0 +1,13 @@
+package com.thizac.thizacplaygroundblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThizacPlaygroundBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThizacPlaygroundBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thizac/thizacplaygroundblog/ThizacPlaygroundBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thizac.thizacplaygroundblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThizacPlaygroundBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
